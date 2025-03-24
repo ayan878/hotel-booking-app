0ef913a3ff66ead29fe11b3b8d4a4e04f02df181
ayan878 <ayanraza197@gmail.com>
2025-03-24 16:59:42 +0530

    playwright test

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -9,9 +9,7 @@ test("should allow the user to sign in", async ({ page }) => {
   await page
     .getByRole("textbox", { name: "Email" })
     .fill("ayanraza197@gmail.com");
-await page
-  .getByRole("textbox", { name: "Password" })
-  .fill("123456");
+  await page.getByRole("textbox", { name: "Password" }).fill("123456");
 
   await page.getByRole("button", { name: "Sign In" }).click();
 
@@ -19,20 +17,32 @@ await page
   await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
   await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
   await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
-
-
-  // Expect a title "to contain" a substring.
-  // await expect(page).toHaveTitle(/Playwright/);
 });
 
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
 
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
+test("should allow the user to register", async ({ page }) => {
+  await page.goto(UI_URL);
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole("heading", { name: "Installation" })
-//   ).toBeVisible();
-// });
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  await page.getByRole("link", { name: " Create an account here?" }).click();
+  await expect(
+    page.getByRole("heading", { name: "Create an Account" })
+  ).toBeVisible();
+
+  await page.locator('input[name="firstName"]').fill("Ayan");
+  await page.locator('input[name="lastName"]').fill("Raza");
+
+  await page
+    .locator('input[ type="email"]')
+    .fill(`ayanraza${Math.floor(Math.random()* 500 - 100) + 1}@gmail.com`);
+
+  await page.locator('input[type="password"]').nth(0).fill("password123"); // First password input
+  await page.locator('input[type="password"]').nth(1).fill("password123"); // confirm password input
+
+  await page.getByRole("button", { name: "Create Account" }).click();
+  await expect(page.getByText("Register Successful")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
